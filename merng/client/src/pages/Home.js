14acf27891b5feca7b7690a84123f12ede35c722
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-21 15:17:00 -0500

    Added like and comment buttons without functionality. Styled 'Recent Posts' h1.

--- merng/client/src/pages/Home.js
+++ merng/client/src/pages/Home.js
@@ -6,12 +6,12 @@ import { Grid } from 'semantic-ui-react';
 import PostCard from '../components/PostCard';
 
 export default function Home() {
-    const { loading, data: { getPosts: posts } } = useQuery(FETCH_POSTS_QUERY);
+    const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
 
     return (
       <Grid columns={3}>
-        <Grid.Row>
-          <hi>Recent Posts</hi>
+        <Grid.Row className="page-title">
+          <h1>Recent Posts</h1>
         </Grid.Row>
         <Grid.Row>
           {loading ? (
@@ -19,7 +19,7 @@ export default function Home() {
           ) : (
             posts &&
             posts.map((post) => (
-              <Grid.Column key={post.id}>
+              <Grid.Column key={post.id} style={{marginBottom: 20}}>
                 <PostCard post={post} />
               </Grid.Column>
             ))
