d3a774572e0e425295c584bd9b978faa99e66eae
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-22 09:54:27 -0500

    Working on singlePost and delete functionality.

--- merng/client/src/pages/Home.js
+++ merng/client/src/pages/Home.js
@@ -9,7 +9,10 @@ import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 export default function Home() {
     const { user } = useContext(AuthContext);
-    const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
+    const {
+    loading,
+    data: { getPosts: posts } = {}
+  } = useQuery(FETCH_POSTS_QUERY);
 
     return (
         <Grid columns={3}>
